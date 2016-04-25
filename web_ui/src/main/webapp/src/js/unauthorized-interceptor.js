ff8d1e456d68a91c8f8c3438b129489db15dbaea
Rupert Smith <rupertlssmith@github.com>
2016-04-25 09:39:29 +0100

    Got 401 interceptor working.

--- web_ui/src/main/webapp/src/js/unauthorized-interceptor.js
+++ web_ui/src/main/webapp/src/js/unauthorized-interceptor.js
@@ -11,6 +11,9 @@ authService.config(function ($httpProvider) {
             $state = $injector.get('$state');
         });
 
+        var loginShown = false;
+        var loginModalPromise = null;
+        
         return {
             responseError: function (rejection) {
                 if (rejection.status !== 401) {
@@ -20,15 +23,29 @@ authService.config(function ($httpProvider) {
                 var deferred = $q.defer();
 
                 console.log("login caused by 401");
-                
-                LoginModalService()
-                    .then(function () {
-                        deferred.resolve($http(rejection.config));
-                    })
-                    .catch(function () {
-                        $state.go('welcome');
-                        deferred.reject(rejection);
+
+                if (!loginShown) {
+                    loginShown = true;
+                    
+                    loginModalPromise = LoginModalService();
+
+                    loginModalPromise
+                        .then(function () {
+                            loginShown = false;
+                            deferred.resolve($http(rejection.config));
+                        })
+                        .catch(function () {
+                            loginShown = false;
+                            $state.go('welcome');
+                            deferred.reject(rejection);
+                        });
+                }
+                else
+                {
+                    loginModalPromise.then(function() {
+                        deferred.resolve($http(rejection.config));                        
                     });
+                }
 
                 return deferred.promise;
             }
