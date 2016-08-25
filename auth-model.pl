c38913418917b5bbb0f111dc7c973d6ef75782d5
Rupert Smith <rupertlssmith@github.com>
2016-08-25 17:19:17 +0100

    Model changes.

--- auth-model.pl
+++ auth-model.pl
@@ -1,5 +1,5 @@
 type_instance(namedRef, view_type, [fields([property(name, string, "name")]), views([])]).
-type_instance(account, entity_type, [fields([unique(key, fields([property(username, string, "username")])), property(password, string, "password"), collection(set, roles, no_parent, fields([component_ref(role, role, false, _)]))]), views([])]).
-type_instance(role, entity_type, [fields([unique(key, fields([property(name, string, "name")])), collection(set, accounts, no_parent, fields([component_ref(account, account, false, _)])), collection(set, permissions, no_parent, fields([component_ref(permission, permission, false, _)]))]), views([])]).
+type_instance(account, entity_type, [fields([unique(key, fields([property(username, string, "username")])), property(password, string, "password"), collection(set, roles, no_parent, fields([component_ref(role, role, false, _, _)]))]), views([])]).
+type_instance(role, entity_type, [fields([unique(key, fields([property(name, string, "name")])), collection(set, accounts, no_parent, fields([component_ref(account, account, false, _, _)])), collection(set, permissions, no_parent, fields([component_ref(permission, permission, false, _, _)]))]), views([])]).
 type_instance(permission, entity_type, [fields([unique(key, fields([property(name, string, "name")]))]), views([])]).
 type_instance(authRequest, component_type, [fields([property(username, string, "username"), property(password, string, "password")]), views([])]).
