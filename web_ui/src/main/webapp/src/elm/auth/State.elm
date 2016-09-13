6217fb65992ae5a38d54743c5cd430009e9ce19c
Rupert Smith <rupertlssmith@github.com>
2016-09-13 16:37:49 +0100

    Adding endpoints and event types.

--- web_ui/src/main/webapp/src/elm/auth/State.elm
+++ web_ui/src/main/webapp/src/elm/auth/State.elm
@@ -1,9 +1,22 @@
 module Auth.State exposing (init)
 
+import Http
+import Http.Decorators
 import Auth.Types exposing (..)
 
 
 init : Model
 init =
     { token = ""
+    }
+
+
+api =
+    "http://localhost:9070/auth"
+
+
+routes =
+    { loginUrl = api ++ "login"
+    , logoutUrl = api ++ "logout"
+    , refreshUrl = api ++ "refresh"
     }
--- web_ui/src/main/webapp/src/elm/auth/Types.elm
+++ web_ui/src/main/webapp/src/elm/auth/Types.elm
@@ -1,5 +1,17 @@
 module Auth.Types exposing (..)
 
+import Http
+
+
+type Msg
+    = HttpError Http.Error
+    | AuthError Http.Error
+    | SetUsername String
+    | SetPassword String
+    | ClickLogIn
+    | GetTokenSuccess String
+    | LogOut
+
 
 type alias Model =
     { token : String
