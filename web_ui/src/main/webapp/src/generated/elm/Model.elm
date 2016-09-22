4e493f8012f42ba8a5d6daf0b2e0748918816ec7
Rupert Smith <rupertlssmith@github.com>
2016-09-22 10:26:30 +0100

    Codegened the model and encoders/decoders for it.

--- web_ui/src/main/webapp/src/generated/elm/Model.elm
+++ web_ui/src/main/webapp/src/generated/elm/Model.elm
@@ -1,4 +1,4 @@
-module Model exposing (..)
+module Model exposing(..)
 
 import Set exposing (Set)
 import Dict exposing (Dict)
@@ -6,30 +6,203 @@ import Json.Decode as Decode exposing (..)
 import Json.Decode.Extra exposing ((|:))
 import Json.Encode as Encode exposing (..)
 
-
-type NamedRef
-    = NamedRef
-        { name : String
-        , name2 : String
-        }
+type NamedRef =
+    NamedRef
+    {
+    name : String
+    }
 
 
 namedRefEncoder : NamedRef -> Encode.Value
 namedRefEncoder (NamedRef model) =
-    Encode.object
-        [ ( "name", Encode.string model.name )
-        ]
+        Encode.object
+            [
+            ( "name", Encode.string model.name )
+            ]
 
 
 namedRefDecoder : Decoder NamedRef
 namedRefDecoder =
     (Decode.succeed
-        (\name name2 ->
+        (\name ->
             NamedRef
-                { name = name
-                , name2 = name2
+                {
+                name = name
                 }
         )
     )
         |: ("name" := Decode.string)
-        |: ("name2" := Decode.string)
+
+
+type AuthRequest =
+    AuthRequest
+    {
+    username : String
+    , password : String
+    }
+
+
+authRequestEncoder : AuthRequest -> Encode.Value
+authRequestEncoder (AuthRequest model) =
+        Encode.object
+            [
+            ( "username", Encode.string model.username )
+            , ( "password", Encode.string model.password )
+            ]
+
+
+authRequestDecoder : Decoder AuthRequest
+authRequestDecoder =
+    (Decode.succeed
+        (\username password ->
+            AuthRequest
+                {
+                username = username
+                ,password = password
+                }
+        )
+    )
+        |: ("username" := Decode.string)
+        |: ("password" := Decode.string)
+
+
+type AuthResponse =
+    AuthResponse
+    {
+    token : String
+    }
+
+
+authResponseEncoder : AuthResponse -> Encode.Value
+authResponseEncoder (AuthResponse model) =
+        Encode.object
+            [
+            ( "token", Encode.string model.token )
+            ]
+
+
+authResponseDecoder : Decoder AuthResponse
+authResponseDecoder =
+    (Decode.succeed
+        (\token ->
+            AuthResponse
+                {
+                token = token
+                }
+        )
+    )
+        |: ("token" := Decode.string)
+
+
+type Account =
+    Account
+    {
+    username : String
+    , password : String
+    , roles : List Role
+    , id : String
+    }
+
+
+accountEncoder : Account -> Encode.Value
+accountEncoder (Account model) =
+        Encode.object
+            [
+            ( "username", Encode.string model.username )
+            , ( "password", Encode.string model.password )
+            , ("roles", model.roles |> List.map roleEncoder |> Encode.list)
+            , ( "id", Encode.string model.id )
+            ]
+
+
+accountDecoder : Decoder Account
+accountDecoder =
+    (Decode.succeed
+        (\username password roles id ->
+            Account
+                {
+                username = username
+                ,password = password
+                ,roles = roles
+                , id = id
+                }
+        )
+    )
+        |: ("username" := Decode.string)
+        |: ("password" := Decode.string)
+        |: ("roles" := Decode.list roleDecoder)
+        |: ("id" := Decode.string)
+
+
+type Role =
+    Role
+    {
+    name : String
+    , accounts : List Account
+    , permissions : List Permission
+    , id : String
+    }
+
+
+roleEncoder : Role -> Encode.Value
+roleEncoder (Role model) =
+        Encode.object
+            [
+            ( "name", Encode.string model.name )
+            , ("accounts", model.accounts |> List.map accountEncoder |> Encode.list)
+            , ("permissions", model.permissions |> List.map permissionEncoder |> Encode.list)
+            , ( "id", Encode.string model.id )
+            ]
+
+
+roleDecoder : Decoder Role
+roleDecoder =
+    (Decode.succeed
+        (\name accounts permissions id ->
+            Role
+                {
+                name = name
+                ,accounts = accounts
+                ,permissions = permissions
+                , id = id
+                }
+        )
+    )
+        |: ("name" := Decode.string)
+        |: ("accounts" := Decode.list accountDecoder)
+        |: ("permissions" := Decode.list permissionDecoder)
+        |: ("id" := Decode.string)
+
+
+type Permission =
+    Permission
+    {
+    name : String
+    , id : String
+    }
+
+
+permissionEncoder : Permission -> Encode.Value
+permissionEncoder (Permission model) =
+        Encode.object
+            [
+            ( "name", Encode.string model.name )
+            , ( "id", Encode.string model.id )
+            ]
+
+
+permissionDecoder : Decoder Permission
+permissionDecoder =
+    (Decode.succeed
+        (\name id ->
+            Permission
+                {
+                name = name
+                , id = id
+                }
+        )
+    )
+        |: ("name" := Decode.string)
+        |: ("id" := Decode.string)
+
+
