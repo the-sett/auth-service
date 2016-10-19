6e9cbe5f7ce875e8a06a75a5fd395a085eef8472
Rupert Smith <rupertlssmith@github.com>
2016-10-19 16:39:39 +0100

    Added some documentation to functions in utils.

--- web_ui/src/main/webapp/src/elm/Utils.elm
+++ web_ui/src/main/webapp/src/elm/Utils.elm
@@ -5,9 +5,22 @@ import Http
 import Auth
 
 
+{-
+   Combines a list of unary tests on some model into a single unary test on the
+   model with the result being the conjunction all of the individual tests.
+-}
+
+
 checkAll : List (model -> Bool) -> model -> Bool
 checkAll checks model =
     List.map (\check -> check model) checks |> List.foldl (&&) True
+
+
+
+{-
+   A defalt HTTP error handler that maps:
+   401 UNAUTHED -> Auth.unauthed
+-}
 
 
 error : Http.Error -> model -> ( model, Cmd msg )
@@ -20,9 +33,22 @@ error httpError model =
             ( model, Cmd.none )
 
 
+
+{-
+   Finds the nth element of a list.
+-}
+
+
 nth : Int -> List a -> Maybe a
 nth k xs =
     List.drop k xs |> List.head
+
+
+
+{-
+   Computes the symetric difference of two dictionaries. The result contains items
+   appearing only in one or other of the inputs.
+-}
 
 
 symDiff : Dict comparable a -> Dict comparable a -> Dict comparable a
