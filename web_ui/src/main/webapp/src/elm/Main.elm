8e35ae21db3ba83faa5fd1c2357ddc66197022b6
Rupert Smith <rupert@thesett.com>
2018-02-06 14:54:32 +0000

    Added the time travel debugger.

--- web_ui/src/main/webapp/src/elm/Main.elm
+++ web_ui/src/main/webapp/src/elm/Main.elm
@@ -1,20 +1,55 @@
 module Main exposing (main)
 
 import RouteUrl as Routing
-import Config
+import TimeTravel.Navigation as TimeTravel
+import Config exposing (config)
 import Top
+    exposing
+        ( delta2url
+        , location2messages
+        , init
+        , update
+        , subscriptions
+        , view
+        , Model
+        , Msg
+        )
 
 
 -- Entry point
 
 
-main : Routing.RouteUrlProgram Never Top.Model Top.Msg
 main =
+    debugMain
+
+
+debugMain =
+    let
+        navApp =
+            Routing.navigationApp
+                { init = init config
+                , subscriptions = subscriptions
+                , update = update
+                , view = view
+                , delta2url = delta2url
+                , location2messages = location2messages
+                }
+    in
+        TimeTravel.program navApp.locationToMessage
+            { init = navApp.init
+            , subscriptions = navApp.subscriptions
+            , update = navApp.update
+            , view = navApp.view
+            }
+
+
+routeMain : Routing.RouteUrlProgram Never Model Msg
+routeMain =
     Routing.program
-        { delta2url = Top.delta2url
-        , location2messages = Top.location2messages
-        , init = Top.init Config.config
-        , view = Top.view
-        , subscriptions = Top.subscriptions
-        , update = Top.update
+        { init = init config
+        , subscriptions = subscriptions
+        , update = update
+        , view = view
+        , delta2url = delta2url
+        , location2messages = location2messages
         }
